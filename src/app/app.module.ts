c7815de10c9f0a2c56caa383f68f199b987c819a
Olia1406 <yurasolia@gmail.com>
2020-07-10 22:09:03 +0300

    JS.Adv.HW.12

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Hw12Component } from './hw12/hw12.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Hw12Component,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  exports: [],
+  entryComponents: []
+})
+export class AppModule { }
